eefb189ba6d7cd9e4175d645c6cf5a2666779441
nieshuming <nieshuming@gcall.com>
2018-11-07 11:14:17 +0800

    add code

---
+++ config-server/src/main/java/com/nsm/cloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.nsm.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by nieshuming on 2018/11/6.
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
